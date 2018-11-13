7af5f5feb3a8c72b30f56612cc224ffab9d9b293
Keir Forster <keirforster@gmail.com>
2018-11-12 23:27:12 -0800

    fix SantaClient conflicts (#6)
    * complete basic api endpoints for admin and child
    
    * create angular project
    
    * add angular components with default layouts
    
    * fix SantaClient conflicts

--- SantaClient/src/app/app-routing.module.ts
+++ SantaClient/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent, pathMatch: 'full' },
@@ -13,9 +11,6 @@ const routes: Routes = [
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
     { path: '**', redirectTo: 'home' },
 ];
-=======
-const routes: Routes = [];
->>>>>>> c02d1316e14faabd828b6f304c68c7d791c670ad
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
