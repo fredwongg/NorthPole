2a1a004adc1e01b571357e7507fc752f4572dd35
Keir Forster <keirforster@gmail.com>
2018-11-15 14:44:09 -0800

    Add validation for SantaClient Login component (#9)
    * complete basic api endpoints for admin and child
    
    * create angular project
    
    * add angular components with default layouts
    
    * fix SantaClient conflicts
    
    * update api endpoint comments
    
    * update client
    
    * add SantaClient login form
    
    * Add validation for SantaClient login component

--- SantaClient/src/app/app.module.ts
+++ SantaClient/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     FontAwesomeModule
   ],
   providers: [],
--- SantaClient/src/app/login/login.component.ts
+++ SantaClient/src/app/login/login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { faSync } from '@fortawesome/free-solid-svg-icons';
+import { LoginViewModel } from './login-view-model';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+    model: LoginViewModel;
+    submitted: Boolean;
+    faSync = faSync; // loading icon
 
-  constructor() { }
+    constructor() {
+        this.model = {} as LoginViewModel;
+        this.submitted = false;
+    }
 
-  ngOnInit() {
-  }
-
+    login(): void {
+        this.submitted = true;
+        console.log(`model: ${this.model}`);
+    }
 }
